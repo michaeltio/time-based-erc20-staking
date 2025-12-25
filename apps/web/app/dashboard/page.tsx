4fd8411ca00cc6db728c2e27d60028ea72272c1a
Michael Tio <77330912+michaeltio@users.noreply.github.com>
2025-12-25 21:43:37 +0700

    add frontend

---
+++ apps/web/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+
+export default function Dashboard() {
+  return (
+    <div className="container mx-auto flex items-center justify-center flex-col min-h-screen ">
+      test
+    </div>
+  );
+}
