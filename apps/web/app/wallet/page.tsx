9813fe1b9d34a29fa132b10af90db3ca064dc688
Michael Tio <77330912+michaeltio@users.noreply.github.com>
2025-12-30 15:00:57 +0700

    add read blaance

--- apps/web/app/wallet/page.tsx
+++ apps/web/app/wallet/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
-import { useAccount } from "wagmi";
+import { useConnection, useChainId } from "wagmi";
 
 import { formatUnits } from "viem";
 import { useERC20Balance } from "@/hooks/contracts/useRewardToken";
 
 export default function Balance() {
-  const { address, status } = useAccount();
+  const { address, status } = useConnection();
+  const chainId = useChainId();
 
   const { data: balance, isLoading, error } = useERC20Balance(address);
 
-  if (!isConnected) return <p>Connect wallet dulu</p>;
+  if (status !== "connected") return <p>Connect wallet dulu</p>;
   if (isLoading) return <p>Loading balance...</p>;
   if (error) return <p>Error ambil balance</p>;
 
-  return <p>Balance: {balance ? formatUnits(balance, 18) : "0"}</p>;
+  return (
+    <>
+      <p>Chain ID: {chainId}</p>
+      <p>User: {address}</p>
+      <p>Balance: {balance ? formatUnits(balance as bigint, 18) : "0"}</p>
+    </>
+  );
 }
--- apps/web/hooks/contracts/useRewardToken.ts
+++ apps/web/hooks/contracts/useRewardToken.ts
@@ -5,6 +5,7 @@ import type { Abi } from "viem";
 import ERC20_ABI from "./RewardToken.json";
 
 export function useERC20Balance(user?: `0x${string}`) {
+  console.log("Address", process.env.NEXT_PUBLIC_REWARD_TOKEN_ADDRESS);
   const contract = useMemo(
     () => ({
       address: process.env.NEXT_PUBLIC_REWARD_TOKEN_ADDRESS as `0x${string}`,
